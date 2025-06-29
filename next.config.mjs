050ff0d73389435f3325d151d1295e425f468b99
Arman <armancurr@proton.me>
2025-06-29 18:41:27 +0530

    export command + updated next config for sevalla

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // enable full static export on `next build`
+  output: 'export',
+
+  // if you need every route to produce its own index.html
+  // uncomment the next line:
+  // trailingSlash: true,
+
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
       },
     ],
   },
